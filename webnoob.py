49805b585dcd67445655fc6c83e96d7417c4e94d
gawasec_03 <132832304+gawasec03@users.noreply.github.com>
2023-05-08 07:15:52 +0800

    Update webnoob.py

--- webnoob.py
+++ webnoob.py
@@ -2,6 +2,7 @@ import os
 import requests
 import argparse
 import socket
+import whois
 
 def main():
     if os.name == 'nt':
@@ -14,13 +15,13 @@ def main():
 ██║███╗██║██╔══╝  ██╔══██╗██║╚██╗██║██║   ██║██║   ██║██╔══██╗
 ╚███╔███╔╝███████╗██████╔╝██║ ╚████║╚██████╔╝╚██████╔╝██████╔╝
  ╚══╝╚══╝ ╚══════╝╚═════╝ ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═════╝                  
-    Made from ChatGPT | by: gawasec03 2023
-    
+    Made from ChatGPT | by: gawasec03 (2023)
+
     """
     print(banner)
 
     # Define command line arguments
-    parser = argparse.ArgumentParser(description="Detect WAF and CMS from domain and subdomains")
+    parser = argparse.ArgumentParser(description="Detect WAF, CMS, and WHOIS information from domain and subdomains")
     parser.add_argument("domain", help="The domain to scan")
     parser.add_argument("-s", "--subdomains", help="A file containing a list of subdomains")
     args = parser.parse_args()
@@ -53,12 +54,21 @@ def main():
         else:
             print(f"No CMS detected on {url}")
 
-        # Get IP address
+       # Get IP address
         try:
             ip = socket.gethostbyname(url)
-            print(f"\u001b[31mIP address of {url}: {ip}\u001b[0m")  # Warna huruf merah
+            print(f"\u001b[32mIP address of {url}: {ip}\u001b[0m")  # Warna huruf hijau
         except socket.gaierror:
             print(f"\u001b[31mUnable to resolve IP address for {url}\u001b[0m")  # Warna huruf merah
+    
+        # Get WHOIS information
+        try:
+            w = whois.whois(url)
+            print(f"\u001b[33mWHOIS information for {url}:\n{w}\u001b[0m")  # Warna huruf kuning
+        except whois.parser.PywhoisError as e:
+            print(f"\u001b[31mUnable to retrieve WHOIS information for {url}: {e}\u001b[0m")  # Warna huruf merah
+        except Exception as e:
+            print(f"\u001b[31mAn error occurred while retrieving WHOIS information for {url}: {e}\u001b[0m")  # Warna huruf merah      
 
 if __name__ == "__main__":
     main()
